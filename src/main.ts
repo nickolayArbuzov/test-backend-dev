5b7c7ea876299b4dff8cb1b9384b43e433b16af7
Nickolay <arbuzov.n@f-case.ru>
2023-03-11 22:19:29 +0300

    feat: add swagger-doc

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
@@ -9,6 +10,16 @@ async function bootstrap() {
     credentials: true
   }})
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Swagger-doc for test-task')
+    .setDescription('The test-task API description')
+    .setVersion('1.0')
+    .addTag('test-task')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
